f8e96632b8556a5c46e40404717ed9158968a4ab
kallemanoop <akallem2@gitam.in>
2025-03-17 14:44:41 +0530

    Bump version: 0.0.13 → 0.0.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="retractometrics",
-    version="0.0.13",
+    version="0.0.14",
     packages=find_packages(),
     install_requires=[
         "pandas",
